9b76aa7888ac70b92c2007d54e13e368aaaa9c01
Blythe Dunham <blythedunham@gmail.com>
2016-05-19 11:05:48 -0700

    update readme and add some comments

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,10 +1,8 @@
+# Favorite uses polymorphism
+# Some day this app would be upgraded so people can share albums and artists and songs
+# This makes it unreasonable to add an attribute favorite. Instead use polymorphic associations 
 class Favorite < ActiveRecord::Base
   belongs_to :user
   belongs_to :favorited, polymorphic: true
-
-
-  def fid
-    [favorited_id,favorited_type].join("|")
-  end
 
 end
